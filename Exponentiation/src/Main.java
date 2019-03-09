7f6651850f898efb076f933addb728b171d81c6c
w3ks0n <3bitxx1996!>
2019-03-09 21:33:21 +0100

    Wersja 4

---
+++ Exponentiation/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, l, last;
+        n = in.nextInt();
+        String zastepcza;
+        int[][] tab = new int[n][2];
+        String[] tabstring = new String [n];
+        for(int i=0; i<n; i++){
+                tab[i][0] = in.nextInt();
+                tab[i][1] = in.nextInt();
+            tabstring[i]=Integer.toString((int)(pow(tab[i][0],tab[i][1])));
+        }
+        for(int i=0; i<n; i++){
+            zastepcza = tabstring[i];
+            l = zastepcza.length();
+            last = zastepcza.charAt(l-1) - 48;
+            System.out.println(last);
+        }
+    }
+}
