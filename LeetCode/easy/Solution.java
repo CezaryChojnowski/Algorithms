485e16c8ed260500731f35a2236199cff2e40977
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-05-30 09:11:45 +0200

    Fizz Buzz - https://leetcode.com/problems/fizz-buzz/

--- LeetCode/easy/Solution.java
+++ LeetCode/easy/Solution.java
@@ -242,5 +242,56 @@ public class Solution {
                 return A[k] + A[k+1] + A[k+2];
         return 0;
     }
+//########################################################################################################################
+//    Write a program that outputs the string representation of numbers from 1 to n.
+//
+//    But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+//    For numbers which are multiples of both three and five output “FizzBuzz”.
+//
+//    Example:
+//
+//    n = 15,
+//
+//    Return:
+//            [
+//            "1",
+//            "2",
+//            "Fizz",
+//            "4",
+//            "Buzz",
+//            "Fizz",
+//            "7",
+//            "8",
+//            "Fizz",
+//            "Buzz",
+//            "11",
+//            "Fizz",
+//            "13",
+//            "14",
+//            "FizzBuzz"
+//            ]
+//########################################################################################################################
+public static List<String> fizzBuzz(int n) {
+    List<String> result = new ArrayList<String>();
+    for(int i=1; i<=n; i++){
+        if(i%5==0 && i%3==0){
+            result.add("FizzBuzz");
+            continue;
+        }
+        else if(i%3==0){
+            result.add("Fizz");
+            continue;
+        }
+        else if(i%5==0){
+            result.add("Buzz");
+            continue;
+        }
+        else{
+            result.add(Integer.toString(i));
+        }
+    }
+    return result;
+}
+
 
 }
